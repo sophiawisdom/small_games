cb5e8d9f8655ea953b7c0334e47d69ccc52d90bf
William Wisdom <william.wisdom@gmail.com>
2019-07-29 08:24:05 -0700

    fix es6 test game

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => {
     <div className="App">
       {/* <GameContainer game="test_game" /> */}
       <br />
-      <GameContainer game="test_dwitter_for_es6" environment="es6" />
+      <GameContainer game="test_game" environment="es6" />
     </div>
   );
 }
