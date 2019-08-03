a45a3ba0d2b1f51701e462cf0abaddee16769e86
William Wisdom <william.wisdom@gmail.com>
2019-08-02 21:21:58 -0700

    Get offscreen working

--- public/js_environments/offscreen_canvas_environment.js
+++ public/js_environments/offscreen_canvas_environment.js
@@ -1,3 +1,4 @@
-const offscreen = document.getElementById("main").transferControlToOffscreen()
+const canvas = document.getElementById("main")
+const offscreen = canvas.transferControlToOffscreen()
 const worker = new Worker(game_path)
-worker.postMessage({ canvas: offscreen }, offscreen)
+worker.postMessage({ canvas: offscreen }, [offscreen])
--- sample_games/offscreen_dwitter_test.js
+++ sample_games/offscreen_dwitter_test.js
@@ -1,35 +1,34 @@
 let animationWorker = null;
+var canvas, canvasContext;
+var time = 0;
+
+var calls = []
+
 self.onmessage = function(e) {
-    console.log("Received message:", e)
-    /*
-  switch (e.data.msg) {
-    case 'start':
-      if (!animationWorker) {
-        importScripts(e.data.origin + '/animation.js');
-        animationWorker = new Animation(e.data.canvas.getContext('2d'));
-      }
-      animationWorker.start();
-      break;
-    case 'stop':
-      animationWorker.stop();
-      break;
-  }
-  */
-}
-
-/*
-function update(t) {
-    canvas.width |= 0
-    canvasContext.fillStyle = "red";
-    for (let i = 1001; i > 0; i--) {
-        const Y = Math.sin(i/159)
-        const xCoord = 900 + 500*i*Math.sin(Y*Math.sin(t/20)*20*Math.sin(t))/900
-        const yCoord = 900 - 800*Math.pow(Y, 2)
-        canvasContext.lineTo(xCoord, yCoord)
+  canvas = e.data.canvas
+  canvasContext = canvas.getContext("2d")
+  console.log("Created canvas", canvas, "and context", canvasContext)
+  setInterval(() => {
+    if (time % 180 === 0) {
+      console.log("Clearing canvas");
+      canvasContext.clearRect(0, 0, 2000, 2000);
     }
-    canvasContext.fill()
+    let now = new Date()
+    calls.push(now)
+    calls = calls.filter(s => (now-s) < 5000)
+    console.log(`${calls.length} calls in the last 5 seconds`)
+    update(time++/60)
+  }, 1000/60)
 }
 
-let time = 0;
-setTimeout(() => update(time++/60), 1000/60)
-*/
+function update(t) {
+  canvasContext.fillStyle = "red";
+  for (let i = 1001; i > 0; i--) {
+      const Y = Math.sin(i/159)
+      const xCoord = 900 + 500*i*Math.sin(Y*Math.sin(t/20)*20*Math.sin(t))/900
+      const yCoord = 900 - 800*Math.pow(Y, 2)
+      canvasContext.lineTo(xCoord, yCoord)
+  }
+  canvasContext.fill()
+}
+
